3f4b80e6fe5d2ee02d6725dc8eebb77e77ad8b64
Harshit-pandey-bit <pandeyharshit287@gmail.com>
2025-07-24 11:11:38 +0530

    i have chnage the text of button

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -3,7 +3,8 @@
 export default function Button() {
   return (
     <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
-      I'm a Button
+    I am a web 
+  
     </button>
   );
 }
